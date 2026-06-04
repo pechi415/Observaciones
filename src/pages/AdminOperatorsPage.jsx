550512685c8e3e4f9d59a11b31ed54a382d85ade
Alexander Ramirez <alexander_ramirez_c@hotmail.com>
2026-06-04 15:14:00 -0500

    fix: add horizontal scroll to operators table for mobile

--- src/pages/AdminOperatorsPage.jsx
+++ src/pages/AdminOperatorsPage.jsx
@@ -203,7 +203,7 @@ export default function AdminOperatorsPage() {
                 </div>
 
                 {/* Tabla */}
-                <div className="bg-white rounded-lg shadow overflow-hidden">
+                <div className="bg-white rounded-lg shadow overflow-x-auto">
                     <table className="min-w-full divide-y divide-gray-200">
                         <thead className="bg-gray-50">
                             <tr>
